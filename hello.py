8212a3cd749d5edabeebbc601808888bbd51fece
Its-Draky <axviro@fp.insjoaquimmir.cat>
2023-10-19 17:02:49 +0200

    db finished

--- hello.py
+++ hello.py
@@ -1,6 +1,33 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, g, render_template
+import sqlite3, os
 
-@app.route('/')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+app = Flask(__name__)
+app.config['DATABASE'] = 'database.db'
+
+basedir = os.path.abspath(os.path.dirname(__file__)) 
+def get_db():
+    sqlite3_database_path =  basedir + "/database.db"
+    con = sqlite3.connect(sqlite3_database_path)
+    # https://docs.python.org/3/library/sqlite3.html#how-to-create-and-use-row-factories
+    con.row_factory = sqlite3.Row
+    return con
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/products/list')
+def product_list():
+
+    try:
+        with get_db() as con:
+            res = con.execute('SELECT * FROM products')
+            products = res.fetchall()
+            return render_template('products/list.html', products=products)
+    except Exception as e:
+        return str(e)
+
+if __name__ == '__main__':
+    app.run(debug=True)
